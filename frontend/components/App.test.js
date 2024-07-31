61964c7ac45fba0b2115af560b52785cfa5b1900
Ahmad Koroma <ahmadkoroma25@outlook.com>
2024-07-31 09:16:44 -0400

    done

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,46 +1,36 @@
-// Write your tests here
-import React from "react"
-import { render, screen, fireEvent } from '@testing-library/react' ;
-import AppFunctional from "./AppFunctional";
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import AppFunctional from './AppFunctional';
+import '@testing-library/jest-dom';
+test('it shows the correct initial coordinates and steps', () => {
+  // Arrange
+  render(<AppFunctional />);
 
-test('render grid and controls', () => {
-  render(<AppFunctional/>)
-  expect(screen.getAllByRole('button').length).toBe(4);
-  expect(screen.getByText('coordinates (2, 2)')).toBeInTheDocument();
-  expect(screen.getByText('you moved 0 times')).toBeInTheDocument();
-
+  // Assert (since there's no action, we only assert here)
+  expect(screen.getByText('Coordinates (2, 2)')).toBeInTheDocument();
+  expect(screen.getByText('You moved 0 times')).toBeInTheDocument();
 });
-test('move the "B" marker', () => {
-  render(<AppFunctional/>);
+
+test('it moves the "B" marker to the left and updates coordinates and steps', () => {
+  // Arrange
+  render(<AppFunctional />);
+  
+  // Act
   fireEvent.click(screen.getByText('LEFT'));
-  expect(screen.getByText('coordinates 1, 2')).toBeInTheDocument
-  expect(screen.getByText('you moved 1 times')).toBeInTheDocument
 
-  fireEvent.click(screen.getByText('UP'));
-  expect(screen.getByText('coordinates 1, 1')).toBeInTheDocument
-  expect(screen.getByText('you moved 2 times')).toBeInTheDocument
-
-
-
-
-
+  // Assert
+  expect(screen.getByText('Coordinates (1, 2)')).toBeInTheDocument();
+  expect(screen.getByText('You moved 1 time')).toBeInTheDocument();
 });
-test('update email input value', () => {
-  render(<AppFunctional/>);
+
+test('it updates email input value', () => {
+  // Arrange
+  render(<AppFunctional />);
+
+  // Act
   const input = screen.getByPlaceholderText('type email');
-  fireEvent.change(input, {target: { value: 'test@examples.com' } });
+  fireEvent.change(input, { target: { value: 'test@example.com' } });
+
+  // Assert
   expect(input.value).toBe('test@example.com');
 });
-test('submit form and displays message', async () => {
-  render(<AppFunctional/>);
-
-  const input = screen.getByPlaceholderText('type email');
-  fireEvent.change(input, { target: { value: 'test@examples.com' } });
-
-  fireEvent.click(screen.getByText('submit'));
-
-  expect(await screen.findByTex(/message/i)).toBeInTheDocument()
-})
-test('sanity', () => {
-  expect(true).toBe(false)
-})
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,35 +1,29 @@
 import React, { useState } from 'react'
-import axios from 'axios'
+import axios from 'axios';
+
 // Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
+const initialMessage = '';
+const initialEmail = '';
+const initialSteps = 0;
+const initialIndex = 4; // the index the "B" is at
 
 export default function AppFunctional(props) {
+  // State management
   const [message, setMessage] = useState(initialMessage);
   const [email, setEmail] = useState(initialEmail);
   const [steps, setSteps] = useState(initialSteps);
   const [index, setIndex] = useState(initialIndex);
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
 
+  // Helper functions
   function getXY() {
     const x = (index % 3) + 1;
-    const y = Math.floor(index /3) + 1;
-    return {x, y};
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
+    const y = Math.floor(index / 3) + 1;
+    return { x, y };
   }
 
   function getXYMessage() {
-    const {x, y} = getXY()
+    const { x, y } = getXY();
     return `Coordinates (${x}, ${y})`;
-
-    
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
   }
 
   function reset() {
@@ -37,52 +31,51 @@ export default function AppFunctional(props) {
     setEmail(initialEmail);
     setSteps(initialSteps);
     setIndex(initialIndex);
-    // Use this helper to reset all states to their initial values.
   }
 
   function getNextIndex(direction) {
     let newIndex = index;
-    if ( direction === 'left' && index % 3 !== 0) newIndex -= 1;
+    if (direction === 'left' && index % 3 !== 0) newIndex -= 1;
     else if (direction === 'right' && index % 3 !== 2) newIndex += 1;
     else if (direction === 'up' && index > 2) newIndex -= 3;
     else if (direction === 'down' && index < 6) newIndex += 3;
     return newIndex;
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
   }
 
   function move(evt) {
     const direction = evt.target.id;
-    const newIndex = getNextIndex(direction)
-    if ( newIndex !== index) {
+    const newIndex = getNextIndex(direction);
+    if (newIndex !== index) {
       setIndex(newIndex);
-      setSteps(steps + 1)
+      setSteps(steps + 1);
+      setMessage('');
+    } else {
+      setMessage(`You can't go ${direction}`);
     }
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
   }
 
   function onChange(evt) {
     setEmail(evt.target.value);
-    // You will need this to update the value of the input.
   }
 
-  function onSubmit(evt) {
+  async function onSubmit(evt) {
     evt.preventDefault();
     const { x, y } = getXY();
-    const payload = { x, y, steps, email }
-    axios.post('http://localhost:9000/api/result', payload)
-    .then(res => setMessage(res.data.message))
-    .catch(err => setMessage(err.response.data.message))
-    // Use a POST request to send a payload to the server.
+    const payload = { x, y, steps, email };
+    try {
+      const res = await axios.post('http://localhost:9000/api/result', payload);
+      setMessage(res.data.message);
+    } catch (err) {
+      setMessage(err.response.data.message);
+    }
+    setEmail(initialEmail)
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">{getXYMessage()}</h3>
-        <h3 id="steps">You moved {steps} times</h3>
+        <h3 id="steps">You moved {steps} {steps === 1 ? 'time' : 'times'}</h3>
       </div>
       <div id="grid">
         {
@@ -104,7 +97,7 @@ export default function AppFunctional(props) {
         <button id="reset" onClick={reset}>reset</button>
       </div>
       <form onSubmit={onSubmit}>
-        <input id="email" type="email" placeholder="type email"></input>
+        <input id="email" type="email" placeholder="type email" value={email} onChange={onChange}></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
